f1250af35f321ba823e4f5b6f3b753f9396b94ee
SalaiZawHtetAung <hzaw9036@gmail.com>
2023-12-28 09:17:38 +0630

    home about contact route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/home";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
